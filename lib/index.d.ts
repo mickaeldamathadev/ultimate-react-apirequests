1c4544406e6886247653b4fcc0fe6a24589459d9
mickaeldamathadev <127761598+mickaeldamathadev@users.noreply.github.com>
2023-11-08 13:14:46 +0100

    v $version

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,14 @@
-import Form from './form/Form';
-import Binary from './form/inputs/Binary';
-import Checkbox from './form/inputs/Checkbox';
-import ConditonnalRenderer from './form/inputs/ConditionnalRenderer';
-import Input from './form/inputs/Input';
-import Select from './form/inputs/Select';
-import SubmitInput from './form/inputs/Submit';
-import TextArea from './form/inputs/TextArea';
-export { Binary, Checkbox, ConditonnalRenderer, Form, Input, Select, SubmitInput, TextArea, };
+type ApiResponse<T> = {
+    data?: T;
+    error?: string;
+    status?: number;
+};
+type NestedRoutes<T> = {
+    [K in keyof T]?: string;
+};
+declare function post<T>(baseUrl: string, nestedRoutes: NestedRoutes<T>, data: T): Promise<ApiResponse<T>>;
+declare function put<T>(baseUrl: string, nestedRoutes: NestedRoutes<T>, data: T | Partial<T>): Promise<ApiResponse<T>>;
+declare function patch<T>(baseUrl: string, nestedRoutes: NestedRoutes<T>, data: Partial<T>): Promise<ApiResponse<T>>;
+declare function get<T>(baseUrl: string, nestedRoutes?: NestedRoutes<T>): Promise<ApiResponse<T>>;
+declare function del<T>(baseUrl: string, nestedRoutes: NestedRoutes<T>): Promise<ApiResponse<void>>;
+export { del, get, patch, post, put };
